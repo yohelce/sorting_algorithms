48114cf9e82b788f7b6021ed6de4f34d17c44d67
Your Name <yohel.cruz.espinoza@gmail.com>
2022-06-18 21:54:37 -0700

    Added 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -30,7 +30,6 @@ void counting_sort(int *array, size_t size)
 		return;
 	/* Finding the max value */
 	max =  max_value(array, size);
-	/* Making an array of counter to each value */
 	count_arr = malloc(sizeof(size_t) * (max + 1));
 	if (!count_arr)
 		return;
@@ -44,7 +43,6 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < max; i++)
 		count_arr[i + 1] += count_arr[i];
 	print_array(count_arr, max + 1);
-	/* Making the array of output */
 	output_arr = malloc(sizeof(size_t) * size);
 	/* If output_arr doesn`t exists we freed count_arr and return */
 	if (!output_arr)
@@ -52,8 +50,6 @@ void counting_sort(int *array, size_t size)
 		free(count_arr);
 		return;
 	}
-	/* Searching the array value into the count_arr, then returns */
-	/* the value -1 and this is the index in the output array */
 	for (i = ((int)size - 1); i >= 0; i--)
 	{
 		count_arr[array[i]] -= 1;
@@ -62,7 +58,6 @@ void counting_sort(int *array, size_t size)
 	/* Assigning to each value of the array the value of the output array */
 	for (i = 0; i < (int)size; i++)
 		array[i] = output_arr[i];
-	/* Realising the arrays made with malloc*/
 	free(count_arr);
 	free(output_arr);
 }
